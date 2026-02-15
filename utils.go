1cbda9cf14c503a6eb9fb55c102c2807cf09499d
Jack <jacksonmayhew9@gmail.com>
2026-02-15 14:50:07 -0800

    move regex compile to package level

--- utils.go
+++ utils.go
@@ -4,9 +4,10 @@ import (
 	"regexp"
 )
 
+var trackIDRegex = regexp.MustCompile(`track/([a-zA-Z0-9]{22})`)
+
 func extractTrackID(url string) string {
-	re := regexp.MustCompile(`track/([a-zA-Z0-9]{22})`)
-	matches := re.FindStringSubmatch(url)
+	matches := trackIDRegex.FindStringSubmatch(url)
 
 	if len(matches) > 1 {
 		return matches[1]
