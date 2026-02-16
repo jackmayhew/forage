25f08e920730e4e4168299e87f2444a1d75ffa24
Jack <jacksonmayhew9@gmail.com>
2026-02-16 09:17:25 -0800

    add single track download and make source track opt-in

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 	countFlag := flag.Int("count", 10, "Number of similar tracks to find")
 	outputFlag := flag.String("output", "./foraged-tracks", "Output directory for foraged tracks")
 	quietFlag := flag.Bool("quiet", false, "Quiet mode - minimal output")
+	onlyFlag := flag.Bool("only", false, "Only download the provided track")
+	includeSourceFlag := flag.Bool("include-source", false, "Include the provided track in the download")
 	setupFlag := flag.Bool("setup", false, "Create config file template")
 	flag.Parse()
 	
@@ -53,8 +55,6 @@ func main() {
 	spotifyClientID := config.SpotifyClientID
 	spotifyClientSecret := config.SpotifyClientSecret
 	lastfmAPIKey := config.LastFmAPIKey
-
-	setQuietMode(*quietFlag)
 
 	if *countFlag > 50 {
 		fmt.Println("Count cannot exceed 50 (Last.fm API limit)")
@@ -100,54 +100,81 @@ func main() {
 	trackName := track.Name
 
 	logInfo("\nFound: %s - %s\n\n", artistName, trackName)
-	logInfo("Finding %d similar tracks on Last.fm...\n", *countFlag)
-
+	if !*onlyFlag {
+		logInfo("Finding %d similar tracks on Last.fm...\n", *countFlag)
+	}
 	// Similar tracks from Last.fm
-	similarTracks, err := getSimilarTracks(lastfmAPIKey, artistName, trackName, *countFlag)
-	if err != nil {
-		fmt.Printf("Error getting similar tracks: %v\n", err)
-		os.Exit(1)
+	var similarTracks []LastFmTrack
+	
+	if !*onlyFlag {
+		var err error
+		similarTracks, err = getSimilarTracks(lastfmAPIKey, artistName, trackName, *countFlag)
+		if err != nil {
+			fmt.Printf("Error getting similar tracks: %v\n", err)
+			os.Exit(1)
+		}
 	}
 
-	logInfo("\nFound %d similar tracks:\n\n", len(similarTracks))
-
-	if len(similarTracks) == 0 {
-		fmt.Println("No similar tracks found. Try a different song.")
-		os.Exit(0)
+	totalToDownload := len(similarTracks)
+	if *onlyFlag || *includeSourceFlag {
+		totalToDownload++
 	}
 
-	for i, t := range similarTracks {
-		logInfo("%d. %s - %s\n", i+1, t.Artist.Name, t.Name)
+	if !*onlyFlag && len(similarTracks) > 0 {
+		logInfo("\nFound %d similar tracks:\n\n", len(similarTracks))
+		for i, t := range similarTracks {
+			logInfo("%d. %s - %s\n", i+1, t.Artist.Name, t.Name)
+		}
 	}
 
-	logInfo("\n--- Starting downloads ---\n\n")
+	logInfo("\n--- Starting downloads (%d total) ---\n\n", totalToDownload)
 
-	// Download tracks
 	var failures []string
 	successCount := 0
 	skippedCount := 0
+	currentIdx := 1
 
-	for i, t := range similarTracks {
-		// Full track info for metadata
-		similarTrackInfo, err := getTrackInfoBySearch(token, t.Artist.Name, t.Name)
-
-		var album, albumArtURL string
-		if err == nil && similarTrackInfo != nil {
-			album = similarTrackInfo.Album.Name
-			if len(similarTrackInfo.Album.Images) > 0 {
-				albumArtURL = similarTrackInfo.Album.Images[0].URL
-			}
-		}
-
-		err = downloadTrack(t.Artist.Name, t.Name, *outputFlag, album, albumArtURL, i+1, len(similarTracks))
+	if *onlyFlag || *includeSourceFlag {
+		artURL := ""
+		if len(track.Album.Images) > 0 { artURL = track.Album.Images[0].URL }
+		
+		err := downloadTrack(artistName, trackName, *outputFlag, track.Album.Name, artURL, currentIdx, totalToDownload)
 		if err != nil {
 			if errors.Is(err, ErrSkipped) {
 				skippedCount++
 			} else {
-				failures = append(failures, fmt.Sprintf("%s - %s", t.Artist.Name, t.Name))
+				failures = append(failures, fmt.Sprintf("%s - %s", artistName, trackName))
 			}
 		} else {
 			successCount++
+		}
+		currentIdx++
+	}
+
+
+	if !*onlyFlag {
+		for _, t := range similarTracks {
+			similarTrackInfo, err := getTrackInfoBySearch(token, t.Artist.Name, t.Name)
+			
+			var album, albumArtURL string
+			if err == nil && similarTrackInfo != nil {
+				album = similarTrackInfo.Album.Name
+				if len(similarTrackInfo.Album.Images) > 0 {
+					albumArtURL = similarTrackInfo.Album.Images[0].URL
+				}
+			}
+
+			err = downloadTrack(t.Artist.Name, t.Name, *outputFlag, album, albumArtURL, currentIdx, totalToDownload)
+			if err != nil {
+				if errors.Is(err, ErrSkipped) {
+					skippedCount++
+				} else {
+					failures = append(failures, fmt.Sprintf("%s - %s", t.Artist.Name, t.Name))
+				}
+			} else {
+				successCount++
+			}
+			currentIdx++
 		}
 	}
 
